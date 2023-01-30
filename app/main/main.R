e8e597e5ae9bd614fa2f62a52f5ae37f13038c78
jkubis96 <jakub.kubis1996@gmail.com>
2023-01-30 14:14:29 +0100

    rep

--- app/main/main.R
+++ app/main/main.R
@@ -20,9 +20,14 @@ library(maps)
 options(shiny.maxRequestSize = 300000*1024^2)
 
 
-
 sys <- read.csv('../../requirements_file/system_files', sep = '=', header = F)
-
+drv <- dbDriver("PostgreSQL")
+connec <- dbConnect(drv, 
+                    dbname = 'jbsda',
+                    host = 'localhost', 
+                    port = '5432',
+                    user = sys$V2[sys$V1 == 'sys4'], 
+                    password = sys$V2[sys$V1 == 'sys4'])
 
 
 
